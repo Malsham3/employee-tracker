b1fda337f0c255b3636625713187818932fd63cf
Mostafa <m.alsham627@gmail.com>
2020-12-09 22:56:31 -0700

    role table

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,17 @@
-DROP DATABASE IF EXISTS employee_DB;
-CREATE DATABASE employee_DB;
-USE employee_DB;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
 CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT REFERENCES department.id,
+  PRIMARY KEY (id),
 );
